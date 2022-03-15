23e4c898c59775158b29ccd81a84b4da2d3060cb
Kieran <kieran@Admins-iMac-2.local>
2022-03-15 12:49:36 -0500

    First try at SMARTS strings

--- pmpc/brush.py
+++ pmpc/brush.py
@@ -14,21 +14,24 @@ class Brush(mb.Compound):
         super(Brush, self).__init__()
 
         # Add parts
-        # self.add(Silane(), label='silane')
-        # self.add(Initiator(), label='initiator')
+        self.add(Silane(), label='silane')
+        self.add(Initiator(), label='initiator')
         chain = mb.recipes.Polymer()
-        chain.add_monomer(MPC(alpha=alpha),indices=[1, 37], separation = .15, replace=False, orientation=[[0,1,0],[0,-1,0]])
-        # chain.add_end_groups(CH3(), index=30, separation=.15)
+        chain.add_monomer(MPC(alpha=alpha),indices=[1, 37], separation = .2, replace=False, orientation=[[0,1,0],[0,-1,0]])
         chain.build(n = chain_length)
         self.add(chain, label='pmpc')
         # self.add(CH3(), label='methyl')
-        # print(self['pmpc'])
-        # mb.force_overlap(self['initiator'], self['initiator']['down'], self['silane']['up'])
-        # mb.force_overlap(self['pmpc'], self['pmpc']['down'], self['initiator']['up'])
-        # mb.force_overlap(self['methyl'], self['methyl']['up'], self['pmpc']['up'])
+
+        self.add(mb.Port(anchor=list(self['pmpc'].particles())[1]), label='pmpc_up')
+        self.add(mb.Port(anchor=list(self['pmpc'].particles())[37]), label='pmpc_down')
+
+        # print(self['methyl'])
+        mb.force_overlap(self['initiator'], self['initiator']['down'], self['silane']['up'])
+        mb.force_overlap(self['pmpc'], self['pmpc_up'], self['initiator']['up'])
+        # mb.force_overlap(self['methyl'], self['methyl']['up'], self['pmpc_down'])
 
         # Make self.port point to silane.bottom_port
-        # self.add(self['silane']['down'], label='down', containment=False)
+        self.add(self['silane']['down'], label='down', containment=False)
 
 if __name__ == "__main__":
     pmpc = Brush()
